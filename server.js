6ac0cec04c838db3dd1899673b7d22df85377e15
kolesnikaleksv <kolesnikaleksv@gmail.com>
2024-07-26 15:22:24 +0300

    Add ejs temlator and temlets

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ const http = require('http');
 const fs = require('fs');
 
 const app = express();
+
+app.set('view engine', 'ejs');
+
 const port = 5000;
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) =>
+  path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 app.get('/', (req, res) => {
-  res.sendFile(createPath('index'));
+  const title = 'Home';
+  res.render(createPath('index'), { title });
 });
 
 app.get('/contacts', (req, res) => {
-  res.sendFile(createPath('contacts'));
+  const title = 'contacts';
+  const contacts = [
+    { name: 'Portfolio', link: 'https://webwizardok.com/' },
+    { name: 'Telegrams', link: 'https://t.me/KolisnykOleksandr' },
+    { name: 'GitHub', link: 'https://github.com/kolesnikaleksv' },
+  ];
+  res.render(createPath('contacts'), { contacts, title });
 });
 
 app.get('/about-us', (req, res) => {
@@ -20,19 +31,23 @@ app.get('/about-us', (req, res) => {
 });
 
 app.get('/posts', (req, res) => {
-  res.sendFile(createPath('posts'));
+  const title = 'posts';
+  res.render(createPath('posts'), { title });
 });
 
 app.get('/posts/:id', (req, res) => {
-  res.sendFile(createPath('post'));
+  const title = 'post';
+  res.render(createPath('post'), { title });
 });
 
 app.get('/add-post', (req, res) => {
-  res.sendFile(createPath('add-post'));
+  const title = 'add post';
+  res.render(createPath('add-post'), { title });
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile(createPath('error'));
+  const title = 'error';
+  res.status(404).render(createPath('error'), { title });
 });
 
 app.listen(port, () => {
